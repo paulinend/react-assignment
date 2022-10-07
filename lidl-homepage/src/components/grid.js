99f262a26e6a03b3a58135a837486aa4fb5c9356
Pauline Ndiaye <aysha.pauline@gmail.com>
2022-10-07 17:10:36 +0200

    handeling click to the parent  component to pass

--- lidl-homepage/src/components/grid.js
+++ lidl-homepage/src/components/grid.js
@@ -1,18 +1,25 @@
+import { useState, useEffect } from 'react';
+
 import roller from '../assets/patines.webp';
 import bike from '../assets/bici.webp';
 import Button from './button';
 import Tabs from './tabs';
 
-const Grid = () => {
+const Grid = ({count, setCount}) => {
+
+  const handleClick = event => {
+    setCount(count + 1);
+  };
+
   return (
     <div className='grid'>
       <div className='card highlighted'>
-        <Button title="Add to Basket"/>
+        <Button handleClick={handleClick} title="Add to Basket"/>
         <Tabs />
-        <img className='furniture-img' src={roller} alt='family rollerblading'></img>
+        <img className='card-img' src={roller} alt='family rollerblading'></img>
       </div>
       <div className='card'>
-        <img className='furniture-img' src={bike} alt='two persons biking'></img>
+        <img className='card-img' src={bike} alt='two persons biking'></img>
       </div>
     </div>
   )
