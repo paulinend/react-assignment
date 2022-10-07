bac14bf9b2c8a31d126742fddae3a8b43a565bb4
Pauline Ndiaye <aysha.pauline@gmail.com>
2022-10-07 17:11:01 +0200

    adding click count on the cart icon

--- lidl-homepage/src/components/header.js
+++ lidl-homepage/src/components/header.js
@@ -1,12 +1,14 @@
-import React from 'react';
 import logo from '../assets/lidl-logo-svg.svg';
-import cart from '../assets/cart-shopping.png';
+import cart from '../assets/icons/cart-shopping.png';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div className='header'>
       <img className='logo' src={logo} alt='lidl logo'></img>
-      <img className='icon-cart' src={cart} alt='shopping cart icon'></img>
+      <div className='cart'>
+        <img className='icon-cart' src={cart} alt='shopping cart icon'></img>
+        {props.count > 0 ? <div className="cercle">{props.count}</div> : null}
+      </div>
     </div>
   )
 }
